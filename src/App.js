8f0b27de5a7e6c8c62c4781fdd5f6821d0598aaf
Andres Felipe <andresfelipedesarrollador@gmail.com>
2021-07-01 20:36:31 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import Pokemon from './components/Pokemon.jsx'
 
 function App() {
+
+  const [elNombreDelPokemon, setElnombredelPokemon] = useState("");
+  const [imagenPokemon, setImagenPokemon] = useState("")
+  const [data, setData] = useState("");
+  const input = useRef("")
+
+
+
+
+
+  function ChangueName() {
+    input.preventDefault();
+    setElnombredelPokemon(input.current.value);
+
+  }
+
+
+  useEffect(() => {
+    async function getAPI() {
+      const url = `https://pokeapi.co/api/v2/pokemon/${elNombreDelPokemon}`;
+      const respuesta = await fetch(url);
+      const json = await respuesta.json();
+      setData(json);
+    }
+    getAPI()
+  }, [elNombreDelPokemon])
+
+
+
+  useEffect(() => {
+    if (data.name) {
+      setElnombredelPokemon(data.name);
+      setImagenPokemon(data.sprites.back_default);
+    } else {
+      setImagenPokemon("")
+    }
+  }, [data])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Nombre del pokemon</label>
+      <div>
+
+        <input ref={input} type="text"  ></input>
+        <button onClick={ChangueName}>Buscar</button>
+      </div>
+
+
+      <Pokemon name={data.name} image={imagenPokemon} />
+
     </div>
   );
 }
---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Pokemon({ name, image }) {
+
+
+
+
+    return (
+        <div className="">
+
+            <h2>{name}</h2>
+            <img src={image} alt="" />
+
+        </div>
+    )
+}
